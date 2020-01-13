e26da7f34fd60fe05afd03ed806d15efe4b14c3c
ThomasHolley <55082849+ThomasHolley@users.noreply.github.com>
2020-01-13 10:32:01 +0100

    First Commit - Redimensionnement avec Excel

---
+++ testEX.php
@@ -0,0 +1,66 @@
+<?php
+require_once 'Classes/PHPExcel.php';
+
+//////////////// Créer Fichier Excel et Insertion/////////////////////////
+
+function createfil()
+{
+    $excel = new PHPExcel();
+
+    $excel->setActiveSheetIndex(0)
+        ->setCellValue('A1', 'Hello')
+        ->setCellValue('B1', 'WOLRD');
+
+    $file = PHPExcel_IOFactory::createWriter($excel, 'Excel2007');
+    $file->save('test.xlsx');
+}
+
+///////////// Récupération données fichier Excel////////////////////
+$excelLoad = PHPExcel_IOFactory::load('test.xlsx');
+$excelLoad->setActiveSheetIndex(0);
+
+$i = 2;
+while ($excelLoad->getActiveSheet()->getCell('A' . $i)->getValue()) {
+
+    $telephone = $excelLoad->getActiveSheet()->getCell('A' . $i)->getValue();
+    $hauteur = $excelLoad->getActiveSheet()->getCell('B' . $i)->getValue();
+    $largeur = $excelLoad->getActiveSheet()->getCell('C' . $i)->getValue();
+    $image = $excelLoad->getActiveSheet()->getCell('D' . $i)->getValue();
+
+
+    $x = $largeur;
+    $y = $hauteur;
+    $dir = $image;
+    $files = glob($dir, GLOB_BRACE);
+
+    echo "test2";
+
+
+    foreach ($files as $dir) {
+        $file = $dir;
+        $size = getimagesize($file);
+        if ($size) {
+            if ($size['mime'] == 'image/jpeg') { # Images en JPEG
+
+                $img_big = imagecreatefromjpeg($file); # On ouvre l'image d'origine
+                $img_new = imagecreate($x, $y); # création de la miniature
+                $img_mini = imagecreatetruecolor($x, $y)
+                    or $img_mini = imagecreate($x, $y);    // copie de l'image, avec le redimensionnement.
+                imagecopyresized($img_mini, $img_big, 0, 0, 0, 0, $x, $y, $size[0], $size[1]);
+                imagejpeg($img_mini, $file);
+            } elseif ($size['mime'] == 'image/png') { # Images en PNG   
+                $img_big = imagecreatefrompng($file); # On ouvre l'image d'origine
+                $img_new = imagecreate($x, $y);   # création de la miniature
+                $img_mini = imagecreatetruecolor($x, $y)
+                    or $img_mini = imagecreate($x, $y); // copie de l'image, avec le redimensionnement.
+                imagecopyresized($img_mini, $img_big, 0, 0, 0, 0, $x, $y, $size[0], $size[1]);
+                imagepng($img_mini, $file);
+            }
+        }
+        echo 'Images ' . $image . ' redimensionné ! <br>';
+    }
+    $i++;
+    echo "test1";
+}
+
+/////////////////////////////////////////////////////////////
