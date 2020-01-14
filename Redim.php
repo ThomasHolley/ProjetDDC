02c7009f2659230053830d7ffd36bf6864692243
ThomasHolley <55082849+ThomasHolley@users.noreply.github.com>
2020-01-14 17:51:55 +0100

    modif code bar

--- Redim.php
+++ Redim.php
@@ -19,13 +19,13 @@ require('fpdf_barcode.php');
 
 $excelLoad = PHPExcel_IOFactory::load('Config.xlsx');
 $excelLoad->setActiveSheetIndex(0);
-$dir = 'Visuel/*';
+$dir = 'Visuel/*';  //Chemin où ce trouve les visuels
 $files = glob($dir, GLOB_BRACE);
-$pdf = new PDF_BARCODE('p', 'mm', array(100,240));
-$pdf->AddPage();
-$pdf->SetFont('Arial', '', 12);
+$pdf = new PDF_BARCODE('p', 'mm', array(100,240)); //creation d'un nouveau pdf avec code bar
+$pdf->AddPage(); //Ajout d'une page sur le PDF
+$pdf->SetFont('Arial', '', 12); // Paramètrage de la police d'écriture
 
-$i = 2;
+$i = 2; // i commence à la deuxième ligne du tableau excel
 while ($excelLoad->getActiveSheet()->getCell('A' . $i)->getValue()) { //Tant que la page excel est chargé, on garde en variable les valeurs des cellules.
 
     $telephone = $excelLoad->getActiveSheet()->getCell('A' . $i)->getValue(); //La variable telephone prend pour valeur la cellule A
@@ -33,15 +33,15 @@ while ($excelLoad->getActiveSheet()->getCell('A' . $i)->getValue()) { //Tant que
     foreach ($files as $dir) { //Boucle sur chaque fichiers du dossier
         $img = $dir;
         $size = getimagesize($img);    //Ajout de la taille du fichier à la variable "size"
-        $part = explode('-', $img);
+        $part = explode('-', $img); // découpage du nom de l'image par des "-"
 
 
-        if ($part[2] == $telephone) {
+        if ($part[2] == $telephone) { // si segment 2 du nom = Cellule A du tableau alors x et y prennent pour valeur B et C
             $y = $excelLoad->getActiveSheet()->getCell('B' . $i)->getValue(); //La variable hauteur prend pour valeur la cellule B
             $x = $excelLoad->getActiveSheet()->getCell('C' . $i)->getValue(); //La variale largeur prend pour valeur la cellule C
-            $pdf->Image($img);
-            $pdf->Text(1, 230, $part[2]); $pdf->Text(20, 230, $part[3]);
-            $pdf->EAN13(1, 210, $part[1], 10, 1, 10);
+            $pdf->Image($img); //Ajout de l'image sur le PDF
+            $pdf->Text(1, 230, $part[2]); $pdf->Text(20, 230, $part[3]); // Ajout du modèle de tel et de la matière sur le pdf
+            $pdf->EAN13(1, 200, $part[1], 20, 0.35, 10); // Ajout d'un code bar du numéro du produit.
 
             if ($size) { //Si la taille a une valeur
                 if ($size['mime'] == 'image/jpeg') { # Images en JPEG
@@ -66,4 +66,4 @@ while ($excelLoad->getActiveSheet()->getCell('A' . $i)->getValue()) { //Tant que
     $i++;
 }
 
-$pdf->Output('', 'I');
+$pdf->Output('Commandes du '.date("m.d.y").'.pdf', 'D'); // Enregistrement du PDF (Dans le navigateur)
