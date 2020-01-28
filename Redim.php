0e50e68034bab20560ffb44d54304ed9b3780b2f
ThomasHolley <55082849+ThomasHolley@users.noreply.github.com>
2020-01-28 15:23:23 +0100

    TEst d'amélioration de qualité + ajout librairie

--- Redim.php
+++ Redim.php
@@ -3,6 +3,8 @@
 require('fpdf_barcode.php');
 require 'vendor/autoload.php';
 
+
+
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 
@@ -23,7 +25,11 @@ $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load('Config.xlsx'); //Initi
 $spreadsheet->setActiveSheetIndex(0); //La feuille de travail Excel "0" est chargé
 $dir = 'Visuel/*';  //Chemin où ce trouve les visuels
 $files = glob($dir, GLOB_BRACE);
-$pdf = new PDF_BARCODE('p', 'mm', array(100, 240)); //creation d'un nouveau pdf avec code bar
+
+$pdf = new PDF_BARCODE('p', 'mm', 'A4'); //creation d'un nouveau pdf avec code bar
+$pdf->SetCompression(0);
+
+
 
 $i = 2; // i commence à la deuxième ligne du tableau excel
 while ($spreadsheet->getActiveSheet()->getCell('B' . $i)->getValue()) { //Tant que la page excel est chargé, on garde en variable les valeurs des cellules.
@@ -32,23 +38,23 @@ while ($spreadsheet->getActiveSheet()->getCell('B' . $i)->getValue()) { //Tant q
     foreach ($files as $dir) { //Boucle sur chaque fichiers du dossier
         $img = $dir;
         $size = getimagesize($img);    //Ajout de la taille du fichier à la variable "size"
-        $newtext = substr($img, 8); //Découpe le chemin et le nom des fichiers pour séléctionner uniquement le nom du fichier
+        $newtext = substr($img, 7); //Découpe le chemin et le nom des fichiers pour séléctionner uniquement le nom du fichier
         $part = explode('-', $newtext); // Découpage du nom de l'image par des "-"
 
-        if ($part[1] == $telephone) { // si segment 2 du nom = Cellule B du tableau alors x et y prennent pour valeur C et D
+        if ($part[2] == $telephone) { // si segment 2 du nom = Cellule B du tableau alors x et y prennent pour valeur C et D
             $y = $spreadsheet->getActiveSheet()->getCell('C' . $i)->getValue(); //La variable hauteur prend pour valeur la cellule C
             $x = $spreadsheet->getActiveSheet()->getCell('D' . $i)->getValue(); //La variale largeur prend pour valeur la cellule D
             $YPX = $y * 4;
             $XPX = $x * 4;
             $pdf->AddPage(); //Ajout d'une page sur le PDF
             $pdf->SetFont('Arial', '', 12); // Paramètrage de la police d'écriture
-            
+
 
             if ($size) { //Si le fichier a une taille
                 if ($size['mime'] == 'image/jpeg') { # Images en JPEG
                     $img_source = imagecreatefromjpeg($img); # On ouvre l'image d'origine
                     $img_dest = imagecreatetruecolor($XPX, $YPX);
-                    imageresolution($img_dest, 300,300);
+                    imageresolution($img_dest, 300, 300);
                     imagecopyresampled($img_dest, $img_source, 0, 0, 0, 0, $XPX, $YPX, $size[0], $size[1]); //l'image est redimensionné
                     imagejpeg($img_dest, $img, 100); // L'image est sauvegardé en JPEG avec une qualité de 100
 
@@ -62,13 +68,26 @@ while ($spreadsheet->getActiveSheet()->getCell('B' . $i)->getValue()) { //Tant q
             $inv0 = mb_strrev($part[0]);
             $inv1 = mb_strrev($part[1]);
             $inv2 = mb_strrev($part[2]);
+            $inv3 = mb_strrev($part[3]);
 
             $pdf->Image($img); //Ajout de l'image sur le PDF
-            $pdf->Text(30, 230, $inv1); // Ajout du modele du tel sur le pdf
-            $pdf->Text(60, 230, $inv2); // Ajout de la matiere du tel sur le pdf
-            $pdf->EAN13(30, 200, $inv0, 20, 0.35, 10); // Ajout d'un code bar du numéro du produit.
+            $pdf->Text(10, 230, $inv0); // Ajout du modele du tel sur le pdf
+            $pdf->Text(20, 230, $inv1); // Ajout du modele du tel sur le pdf
+            $pdf->Text(55, 225, $inv2); // Ajout de la matiere du tel sur le pdf
+            $pdf->Text(60, 231, $inv3); // Ajout de la matiere du tel sur le pdf
+            $pdf->EAN13(30, 190, $part[1], 20, 0.35, 10); // Ajout d'un code bar du numéro du produit.
         }
     }
     $i++;
 }
 $pdf->Output('Commandes du ' . date("d.m.y") . '.pdf', 'I'); // Enregistrement du PDF avec pour nom la date du jour
+
+$path = 'Visuel/'; //ne pas oublier le slash final
+$rep = opendir($path);
+//$i=0;
+while ($file = readdir($rep)) {
+    if ($file != '..' && $file != '.' && $file != '' && $file != '.htaccess') {
+        unlink($path . $file);
+        //$i++;
+    }
+}
---
+++ upload.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Dealer De Coque - Redimensionnement Images</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" href="Index_css.css" />
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+</head>
+
+<body>
+
+    <form action="test1.php">
+        <button class="myButton" type="submit">Redimensionner</button>
+    </form>
+
+</body>
+
+</html>
+
+<?php
+
+extract($_POST);
+$error = array();
+$extension = array("jpeg", "jpg", "png", "gif");
+foreach ($_FILES["images"]["tmp_name"] as $key => $tmp_name) {
+    $file_name = $_FILES["images"]["name"][$key];
+    $file_tmp = $_FILES["images"]["tmp_name"][$key];
+    $ext = pathinfo($file_name, PATHINFO_EXTENSION);
+
+    if (in_array($ext, $extension)) {
+        move_uploaded_file($file_tmp = $_FILES["images"]["tmp_name"][$key], "Visuel/" . $file_name);
+    }
+}
+
+?>
